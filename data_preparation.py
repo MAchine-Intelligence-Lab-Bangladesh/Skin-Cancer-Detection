6c7023942d073abb06768ae2f30a528c6756648a
Afrida-Rahman <afrida.r.samma@gmail.com>
2022-08-05 22:05:15 +0600

    preprocessed ISIC_2019 data

---
+++ data_preparation.py
@@ -0,0 +1,88 @@
+import os
+import shutil
+
+import cv2
+import numpy as np
+from PIL import Image
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.utils.np_utils import to_categorical
+
+
+class data_preparation:
+    def __init__(self):
+        self.data = []
+        self.labels = []
+
+    def data_separation(self, data_list, source, destination, extension=""):
+        allfiles = os.listdir(source)
+
+        for f in allfiles:
+            m = f.split('.')[0]
+            if m in data_list:
+                file = extension + "_" + str(f)
+                print(file)
+                shutil.copy(source + f, destination + file)
+
+    def data_extraction(self):
+        print("appended")
+        self.extract("Basal_cell_carcinoma")
+        self.extract("Nevus")
+        self.extract("Melanoma")
+        feats = np.array(self.data)
+        labels = np.array(self.labels)
+
+        np.save("data/feats_train", feats)
+        np.save("data/labels_train", labels)
+
+        print("extraction done")
+
+    def extract(self, dir_path):
+        path = os.listdir("data/" + dir_path + "/train/")
+        for a in path:
+            image = cv2.imread("data/" + dir_path + "/train/" + a)
+            image_from_array = Image.fromarray(image, 'RGB')
+            size_image = image_from_array.resize((224, 224))
+            self.data.append(np.array(size_image))
+            if dir_path == "Basal_cell_carcinoma":
+                self.labels.append(0)
+            elif dir_path == "Nevus":
+                self.labels.append(1)
+            else:
+                self.labels.append(2)
+            print(a)
+
+    def randomize(self, features, labels):
+        s = np.arange(features.shape[0])
+        np.random.shuffle(s)
+        feats = features[s]
+        labels = labels[s]
+        num_classes = len(np.unique(labels))
+        print(num_classes)
+        return feats, labels
+
+    def normalize(self, x_train, x_test, y_train, y_test):
+        x_train = x_train.astype('float32') / 255
+        x_test = x_test.astype('float32') / 255
+        train_len = len(x_train)
+        test_len = len(x_test)
+
+        y_train = to_categorical(y_train, 3)
+        y_test = to_categorical(y_test, 3)
+        print("normalization done")
+        return x_train, x_test, y_train, y_test
+
+    def image_augmentation(self):
+        trainAug = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+            zoom_range=0.1,  # Randomly zoom image
+            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip=False,  # randomly flip images
+            vertical_flip=False)
+        print("augmentation done")
+        return trainAug
--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import os
-import shutil
-
 import pandas as pd
 
-
-def data_separation(data_list, source, destination, extension=""):
-    allfiles = os.listdir(source)
-
-    for f in allfiles:
-        m = f.split('.')[0]
-        if m in data_list:
-            file = extension + "_" + str(f)
-            print(file)
-            shutil.copy(source + f, destination + file)
+from data_preparation import data_preparation
 
 
-def read_data():
+def ISIC_2019_data_preprocess():
     df = pd.read_csv("data/ISIC_2019_Training_GroundTruth.csv")
     mel_list = df[df['MEL'] == 1.0]['image'].tolist()
     nv_list = df[df['NV'] == 1.0]['image'].tolist()
     bcc_list = df[df['BCC'] == 1.0]['image'].tolist()
-    data_separation(data_list=mel_list, source="data/ISIC_2019_Training_Input/", destination="data/Melanoma/",
-                    extension="MEL")
-    data_separation(data_list=nv_list, source="data/ISIC_2019_Training_Input/", destination="data/Nevus/",
-                    extension="NV")
-    data_separation(data_list=bcc_list, source="data/ISIC_2019_Training_Input/",
-                    destination="data/Basal_cell_carcinoma/", extension="BCC")
+
+    a = data_preparation()
+    a.data_separation(data_list=mel_list, source="data/ISIC_2019_Training_Input/", destination="data/Melanoma/train/",
+                      extension="MEL")
+    a.data_separation(data_list=nv_list, source="data/ISIC_2019_Training_Input/", destination="data/Nevus/train/",
+                      extension="NV")
+    a.data_separation(data_list=bcc_list, source="data/ISIC_2019_Training_Input/",
+                      destination="data/Basal_cell_carcinoma/train/", extension="BCC")
     print(f"melanoma: {len(mel_list)}")
     print(f"nv: {len(nv_list)}")
     print(f"bcc: {len(bcc_list)}")
 
+    a.data_extraction()
+
+
+def main():
+    print("enter")
+    # feats = np.load("data/feats_train.npy")
+    # labels = np.load("data/labels_train.npy")
+    # print("loading done")
+    #
+    # a = data_preparation()
+    # feats, labels = a.randomize(feats, labels)
+    # print("randomization done")
+    #
+    # # splitting 80:20 ratio i.e., 80% for training and 20% for testing purpose
+    # (x_train, x_test) = feats[int(0.2 * len(feats)):], feats[:int(0.2 * len(feats))]
+    # (y_train, y_test) = labels[int(0.2 * len(feats)):], labels[:int(0.2 * len(feats))]
+    # print("spplitting done")
+    #
+    # x_train, x_test, y_train, y_test = a.randomize(x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+    # print("randomize done")
+    #
+    # train_aug = a.image_augmentation()
+
 
 if __name__ == '__main__':
-    read_data()
+    main()
